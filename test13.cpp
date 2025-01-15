5fd688ae6c90034243c8eff1ffe17f338cb6ca8e
Chaewon Jeong <jcw3241@gmail.com>
2025-01-15 23:46:32 +0900

    250115 깊이우선탐색문제 틀

---
+++ test13.cpp
@@ -0,0 +1,40 @@
+// 1. library
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 2. 자료구조
+// graph & visit & sol
+vector<int> graph[100001];
+int visit[100001];
+int sol[100001];
+
+// 3. solve 함수
+void solve() {
+
+}
+// 4. main 함수 : 입력, solve() 호출, 출력
+int main() {
+  FILE* fp;
+  fopen_s(&fp, "test13_input.txt", "r+t");
+
+  int tc;// number of test case
+  fscanf_s(fp, "%d", &tc);
+  for (int i = 0; i < tc; i++) {
+    // 입력 : 초기화, n (vertex의 수) & (n-1)개의 edge
+    // 초기화
+    for(int j = 0; j < 100001; j++) {
+      graph[j].empty(); // empty는 vector의 값을 초기화하는 함수
+    }
+
+    // solve() 호출
+
+    // 출력
+  }
+  fclose(fp);
+  return 0;
+}
+
